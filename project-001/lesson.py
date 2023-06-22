aee7079b26e8b7a59f9efcf303573d137f2143bc
MYSonmez <m.yusufsonmez03@gmail.com>
2023-06-22 23:47:46 +0300

    finish day 1

--- project-001/lesson.py
+++ project-001/lesson.py
@@ -9,4 +9,19 @@
 # print("Hello" + " World") 
 # print("Hello" + " " + "World")
 
-print("Hello World") 
+
+# print("Hello " + input("What is your name?") + "!")
+
+
+# print(len(input("What is your name? ")))
+# OUTPUT // The lenght of input
+
+# name = input("What is your name? ")
+# lenght = len(name)
+# print(lenght)
+# OUTPUT // the lenght of name
+
+
+# variables names cannot start with a number
+# if you are using 2 words in a variable name you should use " _ " between them
+# you should'nt use a function name as a variable name
---
+++ project-001/project.py
@@ -0,0 +1,8 @@
+# BAND NAME GENERATOR
+
+print("Welcome to the 'Band Name Generator'")
+
+place = input("Where are you from?\n")
+name = input("What's your pet's name?\n")
+
+print("Your band name could be: " + place + " " + name)as
