da41b4e129df5707cf2d0e92d1a1305d5414bbe8
MYSonmez <m.yusufsonmez03@gmail.com>
2023-06-22 23:49:59 +0300

    finish day 1

--- project-001/project.py
+++ project-001/project.py
@@ -5,4 +5,4 @@ print("Welcome to the 'Band Name Generator'")
 place = input("Where are you from?\n")
 name = input("What's your pet's name?\n")
 
-print("Your band name could be: " + place + " " + name)as
+print("Your band name could be: " + place + " " + name)
