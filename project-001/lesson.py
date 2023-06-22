8c52f4821ab389dc36f3528ba4c2a7ee93752a7b
MYSonmez <m.yusufsonmez03@gmail.com>
2023-06-22 19:47:11 +0300

    messages

---
+++ project-001/lesson.py
@@ -0,0 +1,12 @@
+# print("Hello World")
+
+
+# print("1. satır\n2. satır") 
+
+
+# print("Hello World") 
+# print("Hello " + "World") 
+# print("Hello" + " World") 
+# print("Hello" + " " + "World")
+
+print("Hello World") 
