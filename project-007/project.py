fc4ef8d5203587d643d5ce767d70e12d89f1f31b
MYSonmez <m.yusufsonmez03@gmail.com>
2023-07-06 22:56:51 +0300

    finished project-7

---
+++ project-007/project.py
@@ -0,0 +1,43 @@
+import random
+import hangman_arts
+import hangman_words
+
+word_list = hangman_words.word_list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+end_of_game = False
+lives = 6
+display = []
+
+print(hangman_arts.logo)
+
+for _ in range(word_length):
+    display += "_"
+
+print(display)
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_arts.stages[lives])
