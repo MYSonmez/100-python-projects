4d1e7aed145226c628090b7e0136ff9d76241b21
MYSonmez <m.yusufsonmez03@gmail.com>
2023-09-03 22:26:14 +0300

    finished project-16

--- project-016/notes.py
+++ project-016/notes.py
@@ -1,23 +1,37 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-kusi = Turtle()
+# kusi = Turtle()
 
-print(kusi)
-kusi.speed(1)
-for n in range(1,10):
-    kusi.shape("turtle")
-    kusi.color("#A8A196")
-    kusi.fillcolor("yellow")
-    kusi.right(60)
-    kusi.setpos((50,100))
-    print(kusi.position())
-    kusi.right(30)
-    kusi.setpos((50,0))
-    kusi.right(90)
-    kusi.setpos((0,0))
-    kusi.right(9)
+# print(kusi)
+# kusi.speed(1)
+# for n in range(1,10):
+#     kusi.shape("turtle")
+#     kusi.color("#A8A196")
+#     kusi.fillcolor("yellow")
+#     kusi.right(60)
+#     kusi.setpos((50,100))
+#     print(kusi.position())
+#     kusi.right(30)
+#     kusi.setpos((50,0))
+#     kusi.right(90)
+#     kusi.setpos((0,0))
+#     kusi.right(9)
 
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+# from prettytable import PrettyTable
+
+# table = PrettyTable()
+# table.align = "l"
+# table.field_names = ["name", "age", "height"]
+# table.add_rows(
+#     [
+#         ["yusuf", "20", "175"],
+#         ["roho", "24", "160"]
+#     ]
+# )
+# print(table)
