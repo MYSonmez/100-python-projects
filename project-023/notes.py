4b631238174385627692995a67504a74d8a9e0ba
Yusuf Sönmez <137165471+MYSonmez@users.noreply.github.com>
2023-10-27 20:53:06 +0300

    finished project-23

---
+++ project-023/notes.py
@@ -0,0 +1,7 @@
+with open("project-023/my_text.txt", "r") as file:
+    contents = file.read()
+    print(contents)
+    
+with open("project-023/new_text.txt", "w") as file:
+    file.write("şlaskdjflşskfjsşdl")
+    
