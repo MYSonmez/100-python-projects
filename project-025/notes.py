1f58b615777371fcc1f13702399a7c905d2811a5
MYSonmez <m.yusufsonmez03@gmail.com>
2023-12-17 18:06:09 +0300

    notes updated

--- project-025/notes.py
+++ project-025/notes.py
@@ -1,19 +1,22 @@
 # numbers = [5, 10, 20]
-# new_numbers = [n*2 for n in numbers]
+# new_numbers = [n * 2 for n in numbers]
 # print(new_numbers)
 
 # name = "yusuf"
 # letter_list = [letter for letter in name]
 # print(letter_list)
 
-# my_list = [number*2 for number in range(1,5) if number*2 < 7]
+# my_list = [number * 2 for number in range(1, 5) if number * 2 < 7]
 # print(my_list)
 
 
-import random
-student_name_list = ["yusuf", "emir", "aziz", "özgür gümüş"]
+# import random
 
-student_grades_dict = {name:random.randint(0, 100) for name in student_name_list}
-print(student_grades_dict)
-succesful_students = {key:value for (key, value) in student_grades_dict.items() if value >= 60}
-print(succesful_students)
+# student_name_list = ["yusuf", "emir", "aziz", "özgür gümüş"]
+
+# student_grades_dict = {name: random.randint(0, 100) for name in student_name_list}
+# print(student_grades_dict)
+# succesful_students = {
+#     key: value for (key, value) in student_grades_dict.items() if value >= 60
+# }
+# print(succesful_students)
